e1ba41dcab808b20ad29cfc171786eee5dfb01c7
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2016-11-09 10:41:02 +0300

    Missed file

---
+++ CrossfitDiary/CrossfitDiary.Web/MvcHelpers/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrossfitDiary.Web.MvcHelpers
+{
+    public static class EnumHelper
+    {
+        public static List<KeyValuePair<string, int>> ToList(Type enumType) 
+        {
+            var list = new List<KeyValuePair<string, int>>();
+
+            foreach (var e in Enum.GetValues(enumType))
+            {
+                list.Add(new KeyValuePair<string, int>(e.ToString(), (int)e));
+            }
+            return list;
+        }
+    }
+}
