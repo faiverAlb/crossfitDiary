333290362c534448516df0833a96a6d9a8034edf
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2016-11-15 10:24:16 +0300

    Add mapping for time

--- CrossfitDiary/CrossfitDiary.Web/Mappings/ViewModelToDomainMappingProfile.cs
+++ CrossfitDiary/CrossfitDiary.Web/Mappings/ViewModelToDomainMappingProfile.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Linq;
+using AutoMapper;
 using CrossfitDiary.Model;
 using CrossfitDiary.Web.ViewModels;
 
@@ -19,9 +21,34 @@ namespace CrossfitDiary.Web.Mappings
                 .ForMember(x => x.RoutineSimple, x => x.MapFrom(y => y.ExercisesToDoList))
                 .ForMember(x => x.RoundCount, x => x.MapFrom(y => y.RoundsCount));
 
+            //            CreateMap<ExerciseViewModel, RoutineSimple>()
+            //                .ForMember(x => x.ExerciseId, x => x.MapFrom(y => y.Id))
+            //                .ForMember(x => x.Id, x => x.Ignore())
+            //                
+            //                .ForMember(x => x.TimeToWork,
+            //                    x =>
+            //                        x.MapFrom(
+            //                            y =>
+            //                                y.ExerciseMeasures.SingleOrDefault(
+            //                                    z => z.ExerciseMeasureType.MeasureType == MeasureTypeViewModel.Time) != null? y.ExerciseMeasures.Single(z => z.ExerciseMeasureType.MeasureType == MeasureTypeViewModel.Time).ExerciseMeasureType.MeasureValue == null
+            //                                    ? null as TimeSpan?
+            //                                    : (TimeSpan?) (object) y.ExerciseMeasures.Single(z => z.ExerciseMeasureType.MeasureType == MeasureTypeViewModel.Time).ExerciseMeasureType.MeasureValue: null));
+
             CreateMap<ExerciseViewModel, RoutineSimple>()
                 .ForMember(x => x.ExerciseId, x => x.MapFrom(y => y.Id))
-                .ForMember(x => x.Id, x => x.Ignore());
+                .ForMember(x => x.Id, x => x.Ignore())
+                .ForMember(x => x.TimeToWork, opt => opt.ResolveUsing<TimeSpanResolver>());
+        }
+    }
+
+    public class TimeSpanResolver : IValueResolver<ExerciseViewModel, RoutineSimple, TimeSpan?>
+    {
+        public TimeSpan? Resolve(ExerciseViewModel source, RoutineSimple destination, TimeSpan? destMember, ResolutionContext context)
+        {
+            ExerciseMeasureViewModel foundTimeMeasure = source.ExerciseMeasures.SingleOrDefault(x => x.ExerciseMeasureType.MeasureType == MeasureTypeViewModel.Time);
+            if (string.IsNullOrEmpty(foundTimeMeasure?.ExerciseMeasureType.MeasureValue))
+                return null;
+            return TimeSpan.Parse(foundTimeMeasure.ExerciseMeasureType.MeasureValue);
         }
     }
 }
