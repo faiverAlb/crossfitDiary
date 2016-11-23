aa623d7e83e9e16e74d75696c0f519f38a23aa57
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2016-11-23 10:19:05 +0300

    Add inputs and js model variables for log workout

--- CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/LogWorkout/LogWorkoutController.js
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/LogWorkout/LogWorkoutController.js
@@ -3,7 +3,17 @@
     self.availableWorkouts = ko.observableArray(parameters.viewModel.availableWorkouts);
     self.selectedWorkout = ko.observable();
     self.isReadOnlyMode = true;
-    
+
+    self.totalRoundsFinished = ko.observable();
+    self.partialRoundsFinished = ko.observable();
+
+    self.distance = ko.observable();
+    self.generalPoints = ko.observable();
+
+    self.wasFinished = ko.observable();
+    self.isRx = ko.observable();
+    self.IsModified = ko.observable();
+
     self.workoutToDisplay = ko.observable();
     self.logWorkout = function () {
         self.service.logWorkout(self.toJSON());
