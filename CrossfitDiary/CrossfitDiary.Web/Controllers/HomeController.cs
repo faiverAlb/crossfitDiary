dedafbb36482050e3ee4afb6011fc2e86a05bb73
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2016-12-05 15:25:46 +0300

    Add initial HomeViewModel with inner view models

--- CrossfitDiary/CrossfitDiary.Web/Controllers/HomeController.cs
+++ CrossfitDiary/CrossfitDiary.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CrossfitDiary.Web.ViewModels;
 
 namespace CrossfitDiary.Web.Controllers
 {
@@ -8,7 +11,49 @@ namespace CrossfitDiary.Web.Controllers
 
         public virtual ActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel()
+            {
+                WeekWorkouts = new List<DayWorkoutViewModel>()
+                {
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now,
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(1),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(2),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(3),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(4),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(5),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                    new DayWorkoutViewModel()
+                    {
+                        Date = DateTime.Now.AddDays(6),
+                        DayOfWeek = DateTime.Now.DayOfWeek.ToString()
+                    },
+                }
+            };
+
+            return View(model: homeViewModel);
         }
 
 
---
+++ CrossfitDiary/CrossfitDiary.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrossfitDiary.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<DayWorkoutViewModel> WeekWorkouts { get; set; } = new List<DayWorkoutViewModel>();
+    }
+
+    public class DayWorkoutViewModel
+    {
+        public DateTime Date { get; set; }
+
+        public string DayOfWeek { get; set; }
+
+        public List<ToLogWorkoutViewModel> Workouts { get; set; }
+
+    }
+}
