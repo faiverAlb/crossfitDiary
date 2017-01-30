f64789ca29ca2d562015f5dc6cdb67d2f0af9701
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2017-01-30 10:38:05 +0300

    Move CrossfitterService to typescript

--- CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/CrossfitterService.js
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/CrossfitterService.js
@@ -1,41 +1,40 @@
-﻿var CrossfitterService = function(pathToApp) {
-    var self = this;
-
-    self.createWorkout = function(model) {
-        modalLoading.init(true);
-        return $.ajax({
-            type: 'POST',
-            url: pathToApp + "api/createWorkout",
-            data: model,
-            async: false
-        }).done(function() {
-            modalLoading.init(false);
-            window.location = "/Home";
-        });
-    };
-
-    self.logWorkout = function(model) {
-        modalLoading.init(true);
-        return $.post({
-            url: pathToApp + "api/logWorkout",
-            data: model
-        }).done(function() {
-            modalLoading.init(false);
-            window.location = "/Home";
-        });
-    };
-
-    self.createAndLogWorkout = function (model) {
-        modalLoading.init(true);
-
-        return $.post({
-            url: pathToApp + "api/createAndLogNewWorkout",
-            data: model
-        }).done(function () {
-            modalLoading.init(false);
-            window.location = "/Home";
-        });
-    };
-
-    return self;
-};
+var modalLoading;
+var CrossfitterService = (function () {
+    function CrossfitterService(pathToApp) {
+        this.pathToApp = pathToApp;
+        this.createWorkout = function (model) {
+            modalLoading.init(true);
+            return $.ajax({
+                type: 'POST',
+                url: this.pathToApp + "api/createWorkout",
+                data: model,
+                async: false
+            }).done(function () {
+                modalLoading.init(false);
+                window.location.href = "/Home";
+            });
+        };
+        this.logWorkout = function (model) {
+            modalLoading.init(true);
+            return $.post({
+                url: this.pathToApp + "api/logWorkout",
+                data: model
+            }).done(function () {
+                modalLoading.init(false);
+                window.location.href = "/Home";
+            });
+        };
+        this.createAndLogWorkout = function (model) {
+            modalLoading.init(true);
+            return $.post({
+                url: this.pathToApp + "api/createAndLogNewWorkout",
+                data: model
+            }).done(function () {
+                modalLoading.init(false);
+                window.location.href = "/Home";
+            });
+        };
+    }
+    return CrossfitterService;
+}());
+//# sourceMappingURL=CrossfitterService.js.map
---
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/CrossfitterService.ts
@@ -0,0 +1,43 @@
+﻿var modalLoading: any;
+class CrossfitterService {
+    
+    constructor(public pathToApp: string) {
+        
+    }
+
+    createWorkout = function (model) {
+        modalLoading.init(true);
+        return $.ajax({
+            type: 'POST',
+            url: this.pathToApp + "api/createWorkout",
+            data: model,
+            async: false
+        }).done(function () {
+            modalLoading.init(false);
+            window.location.href = "/Home";
+        });
+    };
+
+    logWorkout = function (model) {
+        modalLoading.init(true);
+        return $.post({
+            url: this.pathToApp + "api/logWorkout",
+            data: model
+        }).done(function () {
+            modalLoading.init(false);
+            window.location.href = "/Home";
+        });
+    };
+
+    createAndLogWorkout = function (model) {
+        modalLoading.init(true);
+
+        return $.post({
+            url: this.pathToApp + "api/createAndLogNewWorkout",
+            data: model
+        }).done(function () {
+            modalLoading.init(false);
+            window.location.href = "/Home";
+        });
+    };
+}
