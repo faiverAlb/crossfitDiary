5a841a8c0b40de9ceda72e7e6b540a1bdd755f83
faiverAlb <faiver.alb@gmail.com>
2017-02-05 00:37:55 +0300

    Fix styles for calendar

--- CrossfitDiary/CrossfitDiary.Web/Scripts/ko.bindingHandlers.js
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/ko.bindingHandlers.js
@@ -70,93 +70,56 @@ ko.bindingHandlers.inputmask =
 };
 
 
-//ko.bindingHandlers.datepicker = {
-//    init: function (element, valueAccessor, allBindingsAccessor) {
-//        //initialize datepicker with some optional options
-//        var options = allBindingsAccessor().datepickerOptions || {
-//            useCurrent: false,
-//            autoclose: true
-//        };
-//        $(element).datepicker(options);
-//
-//        //when a user changes the date, update the view model
-//        ko.utils.registerEventHandler(element, "changeDate", function (event) {
-//            var value = valueAccessor();
-//            if (ko.isObservable(value)) {
-//                value(event.date);
-//            }
-//        });
-//    },
-//    update: function (element, valueAccessor) {
-//        var value = ko.utils.unwrapObservable(valueAccessor());
-//        $(element).val(value);
-//    }
-//};
-
-//ko.bindingHandlers.datepicker = {
-//    init: function(element, valueAccessor, allBindingsAccessor) {
-//        //initialize datepicker with some optional options
-//        var options = allBindingsAccessor().dateTimePickerOptions || {};
-//        $(element).datetimepicker(options);
-//
-//        //when a user changes the date, update the view model
-//        ko.utils.registerEventHandler(element,
-//            "dp.change",
-//            function(event) {
-//                var value = valueAccessor();
-//                if (ko.isObservable(value)) {
-//                    if (event.date != null && !(event.date instanceof Date)) {
-//                        value(event.date.toDate());
-//                    } else {
-//                        value(event.date);
-//                    }
-//                }
-//            });
-//
-//        ko.utils.domNodeDisposal.addDisposeCallback(element,
-//            function() {
-//                var picker = $(element).data("DateTimePicker");
-//                if (picker) {
-//                    picker.destroy();
-//                }
-//            });
-//    },
-//    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-//
-//        var picker = $(element).data("DateTimePicker");
-//        //when the view model is updated, update the widget
-//        if (picker) {
-//            var koDate = ko.utils.unwrapObservable(valueAccessor());
-//            koDate = koDate || new Date();
-//            //in case return from server datetime i am get in this form for example /Date(93989393)/ then fomat this
-//            koDate = (typeof (koDate) !== 'object') ? new Date(parseFloat(koDate.replace(/[^0-9]/g, ''))) : koDate;
-//
-//            picker.date(koDate);
-//        }
-//    }
-//};
-
-
 ko.bindingHandlers.datepicker = {
     init: function (element, valueAccessor, allBindingsAccessor) {
         //initialize datepicker with some optional options
-        var options = allBindingsAccessor().datepickerOptions || { format: 'DD/MM/YYYY HH:mm' };
+        var options = allBindingsAccessor().dateTimePickerOptions || { format: 'DD/MM/YYYY' };
+        var defaults = {
+            icons: {
+                time: "fa fa-clock-o",
+                date: "fa fa-calendar",
+                up: "fa fa-arrow-up",
+                down: "fa fa-arrow-down",
+                previous: 'fa fa-chevron-left',
+                next: 'fa fa-chevron-right',
+                today: 'fa fa-calendar-check-o',
+                clear: 'fa fa-eraser',
+                close: 'fa fa-times'
+            }
+        };
+        options = $.extend(true, {}, defaults, options);
         $(element).datetimepicker(options);
 
         //when a user changes the date, update the view model
         ko.utils.registerEventHandler(element, "dp.change", function (event) {
             var value = valueAccessor();
             if (ko.isObservable(value)) {
-                value(event.date);
+                if (event.date === false) {
+                    value(null);
+                } else {
+                    value(event.date);
+                }
+            }
+        });
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            var picker = $(element).data("DateTimePicker");
+            if (picker) {
+                picker.destroy();
             }
         });
     },
-    update: function (element, valueAccessor) {
-        var widget = $(element).data("DateTimePicker");
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+        var picker = $(element).data("DateTimePicker");
         //when the view model is updated, update the widget
-        if (widget) {
-            var date = ko.utils.unwrapObservable(valueAccessor());
-            widget.date(date);
+        if (picker) {
+            var koDate = ko.utils.unwrapObservable(valueAccessor());
+
+            //in case return from server datetime i am get in this form for example /Date(93989393)/ then fomat this
+            koDate = (typeof (koDate) !== 'object') ? new Date(parseFloat(koDate.replace(/[^0-9]/g, ''))) : koDate;
+
+            picker.date(koDate);
         }
     }
 };
