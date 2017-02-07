c653b6de09efe69fcece2b04d7151a8b91d153ff
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2017-02-07 08:52:27 +0300

    Add redirecting from non-ww to www

--- CrossfitDiary/CrossfitDiary.Web/Global.asax.cs
+++ CrossfitDiary/CrossfitDiary.Web/Global.asax.cs
@@ -28,5 +28,17 @@ namespace CrossfitDiary.Web
             
             BundleTable.EnableOptimizations = false;
         }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (!Request.Url.Host.StartsWith("www") && !Request.Url.IsLoopback)
+            {
+                UriBuilder builder = new UriBuilder(Request.Url);
+                builder.Host = "www." + Request.Url.Host;
+                Response.StatusCode = 301;
+                Response.AddHeader("Location", builder.ToString());
+                Response.End();
+            }
+        }
     }
 }
