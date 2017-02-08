17455aec6f76db9f2973326d9adfbaf94d2ab213
Albert Gizetdinov <albert.gizetdinov@arcadia.spb.ru>
2017-02-08 16:49:28 +0300

    Fix validation issue with measureValue

--- CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/ExerciseMeasureTypeValue.js
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/ExerciseMeasureTypeValue.js
@@ -2,7 +2,10 @@
     var self = this;
 
     self.measureType = ko.observable(model.measureType);
-    self.measureValue = ko.observable(parseFloat(model.measureValue)).extend({ required: isRequired });
+    self.measureValue = ko.observable().extend({ required: isRequired });
+    if (parseFloat(model.measureValue)) {
+        self.measureValue(parseFloat(model.measureValue));
+    }
     self.measureDesciption = ko.observable(model.description);
     self.shortMeasureDescription = ko.observable(model.shortMeasureDescription);
 
