166d57e63a6e3e3db76c55f2ef4ed775ac0e0434
Albert Gizetdinov <faiver.alb@gmail.com>
2018-01-19 10:33:32 +0300

    Convert ExerciseMeasureType to typescript enum

--- CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/ExerciseMeasureType.js
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/ExerciseMeasureType.js
@@ -1,8 +1,12 @@
-﻿window.Crossfitter = window.Crossfitter || {};
-window.Crossfitter.ExerciseMeasureTypes = {
-    Distance: 0,
-    Count: 1,
-    Time: 2,
-    Weight: 3,
-    Calories: 4
-}
+var Crossfitter;
+(function (Crossfitter) {
+    var ExerciseMeasureTypes;
+    (function (ExerciseMeasureTypes) {
+        ExerciseMeasureTypes[ExerciseMeasureTypes["Distance"] = 0] = "Distance";
+        ExerciseMeasureTypes[ExerciseMeasureTypes["Count"] = 1] = "Count";
+        ExerciseMeasureTypes[ExerciseMeasureTypes["Time"] = 2] = "Time";
+        ExerciseMeasureTypes[ExerciseMeasureTypes["Weight"] = 3] = "Weight";
+        ExerciseMeasureTypes[ExerciseMeasureTypes["Calories"] = 4] = "Calories";
+    })(ExerciseMeasureTypes = Crossfitter.ExerciseMeasureTypes || (Crossfitter.ExerciseMeasureTypes = {}));
+})(Crossfitter || (Crossfitter = {}));
+//# sourceMappingURL=ExerciseMeasureType.js.map
---
+++ CrossfitDiary/CrossfitDiary.Web/Scripts/Crossfitter/ExerciseMeasureType.ts
@@ -0,0 +1,9 @@
+﻿module Crossfitter {
+    export enum ExerciseMeasureTypes {
+        Distance= 0,
+        Count = 1,
+        Time = 2,
+        Weight = 3,
+        Calories = 4
+    }
+}
