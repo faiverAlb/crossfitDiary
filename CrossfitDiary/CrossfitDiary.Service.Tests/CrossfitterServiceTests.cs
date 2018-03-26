30def57a6b141d742c303fbaea622e8730f0d2e2
Albert Gizetdinov <faiver.alb@gmail.com>
2018-03-26 09:02:07 +0300

    Add positive case test

--- CrossfitDiary/CrossfitDiary.Service.Tests/CrossfitterServiceTests.cs
+++ CrossfitDiary/CrossfitDiary.Service.Tests/CrossfitterServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using CrossfitDiary.DAL.EF.Repositories;
 using CrossfitDiary.Model;
@@ -10,19 +11,29 @@ namespace CrossfitDiary.Service.Tests
     [TestFixture]
     public class CrossfitterServiceTests
     {
+        private RoutineComplex GetRoutineComplex()
+        {
+            return new RoutineComplex()
+            {
+                CreatedBy = new ApplicationUser(),
+                ComplexType = RoutineComplexType.AMRAP,
+                RoutineSimple = new List<RoutineSimple>(),
+                Id = 123
+            };
+        }
+
         [Test]
         public void FindDefaultOrExistingWorkout_NoneWorkoutToCheck_MethodReturns_0()
         {
             // Arrange
             IRoutineComplexRepository stubRoutineComplexRepository = A.Fake<IRoutineComplexRepository>();
-            CrossfitterService crossfitterService = new CrossfitterService(stubRoutineComplexRepository, null, null, null);
-            var routineComplexToSave = new RoutineComplex()
-            {
-                CreatedBy = new ApplicationUser()
-            };
-            
+            CrossfitterService crossfitterService =
+                new CrossfitterService(stubRoutineComplexRepository, null, null, null);
+            A.CallTo(() => stubRoutineComplexRepository.GetMany(A<Expression<Func<RoutineComplex, bool>>>._))
+                .Returns(new RoutineComplex[0]);
+
             // Act
-            int actual = crossfitterService.FindDefaultOrExistingWorkout(routineComplexToSave);
+            int actual = crossfitterService.FindDefaultOrExistingWorkout(GetRoutineComplex());
 
             // Assert
             Assert.AreEqual(0, actual);
@@ -35,20 +46,31 @@ namespace CrossfitDiary.Service.Tests
             // Arrange
             IRoutineComplexRepository stubRoutineComplexRepository = A.Fake<IRoutineComplexRepository>();
 
-            var routineComplexs = new RoutineComplex[1]{new RoutineComplex(){ ComplexType = RoutineComplexType.E2MOM}};
-            A.CallTo(() => stubRoutineComplexRepository.GetMany(A<Expression<Func<RoutineComplex, bool>>>._)).Returns(routineComplexs);
-            CrossfitterService crossfitterService = new CrossfitterService(stubRoutineComplexRepository, null, null, null);
-            var routineComplexToSave = new RoutineComplex()
-            {
-                CreatedBy = new ApplicationUser(),
-                ComplexType = RoutineComplexType.AMRAP
-            };
+            RoutineComplex routineComplexToSave = GetRoutineComplex();
+            routineComplexToSave.ComplexType++;
+            A.CallTo(() => stubRoutineComplexRepository.GetMany(A<Expression<Func<RoutineComplex, bool>>>._)).Returns(new[] { routineComplexToSave });
             
             // Act
-            int actual = crossfitterService.FindDefaultOrExistingWorkout(routineComplexToSave);
+            CrossfitterService crossfitterService = new CrossfitterService(stubRoutineComplexRepository, null, null, null);
+            int actual = crossfitterService.FindDefaultOrExistingWorkout(GetRoutineComplex());
 
             // Assert
             Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void FindDefaultOrExistingWorkout_RepositoryContainsSameRoutine_MethodReturnsNonZeroId()
+        {
+            // Arrange
+            IRoutineComplexRepository stubRoutineComplexRepository = A.Fake<IRoutineComplexRepository>();
+            A.CallTo(() => stubRoutineComplexRepository.GetMany(A<Expression<Func<RoutineComplex, bool>>>._)).Returns(new[] { GetRoutineComplex() });
+
+            // Act
+            CrossfitterService crossfitterService = new CrossfitterService(stubRoutineComplexRepository, null, null, null);
+            int actual = crossfitterService.FindDefaultOrExistingWorkout(GetRoutineComplex());
+
+            // Assert
+            Assert.NotZero(actual);
         }
     }
 }
