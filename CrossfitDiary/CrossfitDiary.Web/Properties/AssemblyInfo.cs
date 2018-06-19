d3847f01ebd2d77812ea3f0387d191db6c9d5a42
Albert Gizetdinov <faiver.alb@gmail.com>
2018-06-19 16:10:28 +0300

    Fix version: 0.2.9

--- CrossfitDiary/CrossfitDiary.Web/Properties/AssemblyInfo.cs
+++ CrossfitDiary/CrossfitDiary.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GAlb")]
 [assembly: AssemblyProduct("CrossfitDiary.Web")]
-[assembly: AssemblyCopyright("Copyright © Alb 2016")]
+[assembly: AssemblyCopyright("Copyright © Alb 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.8.0")]
-[assembly: AssemblyFileVersion("0.2.8.0")]
+[assembly: AssemblyVersion("0.2.9.0")]
+[assembly: AssemblyFileVersion("0.2.9.0")]
