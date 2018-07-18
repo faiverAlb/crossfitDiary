5b533c47ccf6e3c9cdf2d25004f0f0624834da74
Albert Gizetdinov <faiver.alb@gmail.com>
2018-07-18 09:35:27 +0300

    Add context and libraries

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.DAL.EF/WorkouterContext.cs
@@ -0,0 +1,25 @@
+﻿using CrossfitDiaryCore.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrossfitDiaryCore.DAL.EF
+{
+    public class WorkouterContext: IdentityDbContext
+    {
+        
+    }
+
+
+    public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDbContext()
+        {
+
+        }
+        public IdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+
+}
