bd993e8787ce0abffdc9638aa83addbf22ef70a0
Albert Gizetdinov <faiver.alb@gmail.com>
2018-08-14 10:48:44 +0300

    Add markup for vue

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/boot.ts
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/boot.ts
@@ -1,8 +1,10 @@
 ﻿//import fontawesome from '@fortawesome/fontawesome-free/js/all';
 import 'bootstrap';
 import './style/app.scss';
+import Vue from 'vue';
 
-
-class boot {
-  
-}
+debugger;
+new Vue({
+  el: '#home-page-container',
+  template: "test"
+});
