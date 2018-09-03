caf5128df7d64f4fb2b6dfd66adc362e3f02168e
Albert Gizetdinov <faiver.alb@gmail.com>
2018-09-03 09:20:43 +0300

    Initial display of exercises

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/exercise-display-component.vue
@@ -0,0 +1,57 @@
+﻿<template>
+  <div>
+    <div class="exercise">
+      <span>{{model.title}}</span>
+      <span class="do-unbroken-info" v-if="model.isDoUnbroken">(unbroken)</span>
+      :
+      <!-- ko foreach:exerciseMeasures-->
+      <!-- ko if:exerciseMeasureType.measureValue != null && exerciseMeasureType.measureValue != "" -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Count)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Distance)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> m</span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Weight)-->
+      (
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> kgs</span>)
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Calories)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> calories </span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Height)-->
+      (
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> cm</span>)
+      <!-- /ko -->
+      <!-- /ko -->
+      <!-- /ko -->
+      <!-- ko if: isNewWeightMaximum -->
+      <span class="new-record" title="Person new weight maximum!">
+        <span class="badge badge-success">new!</span>
+        <span class="badge badge-warning" data-bind="text:addedToMaxWeightString"></span>
+      </span>
+      <!-- /ko -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { ExerciseViewModel } from "../models/viewModels/ExerciseViewModel";
+
+@Component({
+  components: {}
+})
+export default class ExerciseDisplayComponent extends Vue {
+
+   @Prop() 
+   model: ExerciseViewModel;
+}
+</script>
+
+<style>
+</style>
--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/workout-display-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/workout-display-component.vue
@@ -4,72 +4,26 @@
             <span class="workout-title">
                 <span>{{workoutViewModel.workoutTypeDisplay}}</span>:
                 <span v-if="workoutViewModel.IsForTime()">
-                    <span>{{workoutViewModel.roundsCount}} rounds</span>
+                    {{workoutViewModel.roundsCount}} rounds
                 </span>
                 <span  v-if="workoutViewModel.IsAMRAP()">
                     {{workoutViewModel.timeToWork}}
                 </span>
             </span>
-            <span class="cap-time-info" data-bind="if:timeCap">
-                <span>Cap: </span>
-                <span data-bind="text:timeCap"></span>
+            <span class="cap-time-info" v-if="workoutViewModel.timeCap">
+                Cap: {{workoutViewModel.timeCap}}
             </span>
         </span>
-        <div class="workout-exercises pl-3 pt-1">
-            <!-- ko foreach:exercisesToDoList-->
-            <div class="exercise">
-                <span data-bind="text:title"></span>
-                <!-- ko if: isDoUnbroken-->
-                <span class="do-unbroken-info">(unbroken)</span>
-                <!-- /ko -->
-                :
-                <!-- ko foreach:exerciseMeasures-->
-                <!-- ko if:exerciseMeasureType.measureValue != null && exerciseMeasureType.measureValue != "" -->
-                <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Count)-->
-                <span data-bind="text:exerciseMeasureType.measureValue"></span>
-                <!-- /ko -->
-                <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Distance)-->
-                <span data-bind="text:exerciseMeasureType.measureValue"></span>
-                <span> m</span>
-                <!-- /ko -->
-                <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Weight)-->
-                (
-                <span data-bind="text:exerciseMeasureType.measureValue"></span>
-                <span> kgs</span>)
-                <!-- /ko -->
-                <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Calories)-->
-                <span data-bind="text:exerciseMeasureType.measureValue"></span>
-                <span> calories </span>
-                <!-- /ko -->
-                <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Height)-->
-                (
-                <span data-bind="text:exerciseMeasureType.measureValue"></span>
-                <span> cm</span>)
-                <!-- /ko -->
-                <!-- /ko -->
-                <!-- /ko -->
-                <!-- ko if: isNewWeightMaximum -->
-                <span class="new-record" title="Person new weight maximum!">
-                    <span class="badge badge-success">new!</span>
-                    <span class="badge badge-warning" data-bind="text:addedToMaxWeightString"></span>
-                </span>
-                <!-- /ko -->
+        <div class="workout-exercises pl-3 pt-1" v-for="exercise in workoutViewModel.exercisesToDoList" :key="exercise.id">
+            <ExerciseDisplayComponent :model="exercise"></ExerciseDisplayComponent>
+            <div class="rest-between-rounds" v-if="workoutViewModel.restBetweenRounds">
+                <span>Rest: {{workoutViewModel.restBetweenRounds}}</span>
             </div>
-            <!-- /ko -->
-
-            <!-- ko if: restBetweenRounds-->
-            <div class="rest-between-rounds">
-                <span>Rest: </span>
-                <span data-bind="text: restBetweenRounds"></span>
-            </div>
-            <!-- /ko -->
         </div>
         <div class="inner-workouts pl-3 pt-1">
-            <!-- ko foreach:children-->
-            <div class="inner-workout">
-                <div data-bind="template: { name: 'workout-display-template', workoutViewModel: $data }"></div>
+            <div class="inner-workout"  v-for="childWorkout in workoutViewModel.children" :key="childWorkout.id">
+                <WorkoutDisplayComponent :workoutViewModel="childWorkout"></WorkoutDisplayComponent>
             </div>
-            <!-- /ko -->
         </div>
     </div>
 
@@ -78,8 +32,10 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { WorkoutViewModel } from "../models/viewModels/WorkoutViewModel";
+import ExerciseDisplayComponent from "./exercise-display-component.vue";
+
 @Component({
-  components: {}
+  components: {ExerciseDisplayComponent}
 })
 export default class WorkoutDisplayComponent extends Vue {
   @Prop() workoutViewModel: WorkoutViewModel;
