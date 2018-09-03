22d0e1472ea8624016fa95e25331760e65fd1ef5
Albert Gizetdinov <faiver.alb@gmail.com>
2018-09-03 10:16:22 +0300

    Show measures

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/exercise-display-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/exercise-display-component.vue
@@ -4,37 +4,13 @@
       <span>{{model.title}}</span>
       <span class="do-unbroken-info" v-if="model.isDoUnbroken">(unbroken)</span>
       :
-      <!-- ko foreach:exerciseMeasures-->
-      <!-- ko if:exerciseMeasureType.measureValue != null && exerciseMeasureType.measureValue != "" -->
-      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Count)-->
-      <span data-bind="text:exerciseMeasureType.measureValue"></span>
-      <!-- /ko -->
-      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Distance)-->
-      <span data-bind="text:exerciseMeasureType.measureValue"></span>
-      <span> m</span>
-      <!-- /ko -->
-      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Weight)-->
-      (
-      <span data-bind="text:exerciseMeasureType.measureValue"></span>
-      <span> kgs</span>)
-      <!-- /ko -->
-      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Calories)-->
-      <span data-bind="text:exerciseMeasureType.measureValue"></span>
-      <span> calories </span>
-      <!-- /ko -->
-      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Height)-->
-      (
-      <span data-bind="text:exerciseMeasureType.measureValue"></span>
-      <span> cm</span>)
-      <!-- /ko -->
-      <!-- /ko -->
-      <!-- /ko -->
-      <!-- ko if: isNewWeightMaximum -->
-      <span class="new-record" title="Person new weight maximum!">
-        <span class="badge badge-success">new!</span>
-        <span class="badge badge-warning" data-bind="text:addedToMaxWeightString"></span>
+      <span v-for="measure in model.exerciseMeasures" :key="measure.exerciseMeasureType.measureType">
+        <ExerciseMeasureComponent :model="measure"></ExerciseMeasureComponent>
       </span>
-      <!-- /ko -->
+      <span class="new-record" title="Person new weight maximum!" v-if="model.isNewWeightMaximum">
+        <span class="badge badge-success">new!</span>
+        <span class="badge badge-warning" >{{model.addedToMaxWeightString}}</span>
+      </span>
     </div>
   </div>
 </template>
@@ -42,14 +18,16 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { ExerciseViewModel } from "../models/viewModels/ExerciseViewModel";
-
+import ExerciseMeasureComponent from "./exercise-measure-component.vue";
 @Component({
-  components: {}
+  components: {
+    ExerciseMeasureComponent
+  }
 })
 export default class ExerciseDisplayComponent extends Vue {
 
-   @Prop() 
-   model: ExerciseViewModel;
+    @Prop() 
+    model: ExerciseViewModel;
 }
 </script>
 
--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/models/viewModels/ExerciseMeasureViewModel.ts
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/models/viewModels/ExerciseMeasureViewModel.ts
@@ -1,14 +1,60 @@
 ﻿  import Serializable = General.Serializable;
   import {ExerciseMeasureTypeViewModel} from "./ExerciseMeasureTypeViewModel";
+  import {ExerciseMeasureType} from "./ExerciseMeasureType";
+
   export  class ExerciseMeasureViewModel implements Serializable<ExerciseMeasureViewModel>{
+    
 
     exerciseMeasureType: ExerciseMeasureTypeViewModel | null = null;
+    displayMeasure: string;
 
     constructor(params?: { exerciseMeasureType: ExerciseMeasureTypeViewModel }) {
       if (params == null) {
         return;
       }
       this.exerciseMeasureType = params.exerciseMeasureType;
+      this.displayMeasure = this.setDisplayByMeasureType(this.exerciseMeasureType.measureType);
+    }
+
+    setDisplayByMeasureType = (measure: ExerciseMeasureType): string  => {
+      switch (measure) {
+      case ExerciseMeasureType.Distance:
+        return "m";
+      case ExerciseMeasureType.Count:
+        return "";
+      case ExerciseMeasureType.Weight:
+        return "kgs";
+      case ExerciseMeasureType.Calories:
+        return "calories";
+      case ExerciseMeasureType.Height:
+        return "cm";
+      default:
+        return "";
+      }
+
+      /*<!-- ko if:exerciseMeasureType.measureValue != null && exerciseMeasureType.measureValue != "" -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Count)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Distance)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> m</span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Weight)-->
+      (
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> kgs</span>)
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Calories)-->
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> calories </span>
+      <!-- /ko -->
+      <!-- ko if:exerciseMeasureType.measureType == @((int)MeasureTypeViewModel.Height)-->
+      (
+      <span data-bind="text:exerciseMeasureType.measureValue"></span>
+      <span> cm</span>)
+      <!-- /ko -->
+      <!-- /ko --> */
     }
 
     deserialize(input: any): ExerciseMeasureViewModel {
