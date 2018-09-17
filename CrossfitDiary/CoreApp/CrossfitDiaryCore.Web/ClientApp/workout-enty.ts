68c9c1e22e30c4255a217d145a65dd9316c5d902
Albert Gizetdinov <faiver.alb@gmail.com>
2018-09-17 09:53:48 +0300

    Add initial structure for workout page

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-enty.ts
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-enty.ts
@@ -1,1 +1,31 @@
-﻿
+﻿import Vue from "vue";
+
+import CrossfitterService from "./CrossfitterService";
+const apiService = new CrossfitterService();
+
+import { dom } from "@fortawesome/fontawesome-svg-core";
+
+import Spinner from "vue-spinner-component/src/Spinner.vue";
+import { SpinnerModel } from "./models/viewModels/SpinnerModel";
+import { ErrorAlertModel } from "./models/viewModels/ErrorAlertModel";
+import ErrorAlertComponent from "./components/error-alert-component.vue";
+
+new Vue({
+  el: "#ManageWorkoutPageContainer",
+  template: `
+   <div class="manage-workout-container my-2">
+      <div>navigation header here</div>
+      <div>navigation body here</div>
+  </div>
+    `,
+  components: {
+    Spinner,
+    ErrorAlertComponent
+  },
+  data() {
+    return {
+      errorAlertModel: new ErrorAlertModel()
+    };
+  },
+  mounted() {}
+});
