9929b78747fd1f6a56d8ab3746fd5fb0543255ca
Albert Gizetdinov <faiver.alb@gmail.com>
2018-10-25 07:49:03 +0300

    Add UI

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/fortime-edit-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/fortime-edit-component.vue
@@ -1,7 +1,7 @@
 ﻿<template>
   <div class="routine-complex-info">
     <div class="pt-2 general-info-container">
-      <div class="row" data-bind="if: _canSeeTimeCap">
+      <div class="row">
         <div class="col-lg-3 time-cap-container pb-2">
           <label for="timeCapInput" class="sr-only">Time cap:</label>
           <div class="input-group">
@@ -14,28 +14,31 @@
           </div>
         </div>
       </div>
-      <div class="row" data-bind="if: _canSeeTimeToWork">
+      <div class="row">
         <div class="col-lg-3 pb-2">
-          <label for="timeInput" class="sr-only">Time to work:</label>
-          <div class="input-group">
+          <label for="roundsInput" class="sr-only">Rounds count:</label>
+          <div class="input-group ">
             <div class="input-group-prepend">
-              <div class="input-group-text"><i class="far fa-clock"></i></div>
+              <div class="input-group-text">
+                <font-awesome-icon :icon="['fas','hashtag']"></font-awesome-icon>
+              </div>
             </div>
-            <input type="text" pattern="[0-9]*" inputmode="numeric" class="form-control" id="timeInput" placeholder="Time to work" data-bind="validationElement: _timeToWork, inputmask:{value: _timeToWork, mask:'99:59',definitions: { '5': { validator: '[0-5]' } }}">
+            <input type="number" pattern="[0-9]*" inputmode="numeric" min="1" class="form-control " id="roundsInput" placeholder="Rounds count" data-bind="validationElement: _roundsCount, value: _roundsCount, valueUpdate: 'afterkeydown'">
           </div>
         </div>
-
       </div>
     </div>
+  </div>
   </div>
 </template>
 
 <script lang="ts">
 /* Font awesome icons */
 import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons/faHashtag";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-library.add(faClock);
+library.add(faClock, faHashtag);
 /**/
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { Vue, Component, Prop } from "vue-property-decorator";
