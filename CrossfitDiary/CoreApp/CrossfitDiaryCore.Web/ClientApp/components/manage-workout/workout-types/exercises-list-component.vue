fabf604c5a02b109568ea5a43ea6136745c51cc2
Albert Gizetdinov <faiver.alb@gmail.com>
2018-11-13 10:48:49 +0300

    Add initial UI for selected exercises

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/exercises-list-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/exercises-list-component.vue
@@ -8,7 +8,67 @@
         No exercises selected
       </div>
       <div v-for="(exercise,index) in exercisesToDo" :key="`${exercise.id}-${index}`">
-        {{exercise.title}}
+        <div class="simple-routine-item">
+          <div class="form-row">
+            <div class="form-group my-1 col-lg-auto">
+              <div class="input-group">
+                <div class="form-control exercise-title">
+                  <span>{{exercise.title}}</span>
+                  <!-- ko if: _isDoUnbroken-->
+                  <span class="do-unbroken-info badge badge-warning">do unbroken</span>
+                  <!-- /ko -->
+
+                </div>
+                <div class="input-group-append">
+
+                  <button type="button" class="btn btn-outline-secondary dropdown-toggle  dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="sr-only">Toggle Dropdown</span>
+                  </button>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#" data-bind="click: changeUnbrokenState"> <input type="checkbox" data-bind="checked: _isDoUnbroken"> Unbroken</a>
+                    <div role="separator" class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#" data-bind="click:function(){$parent.moveSimpleRoutineUp($index());}, css:{disabled: $index() == 0}"> <i class="fas fa-long-arrow-alt-up text-info"></i> Move up </a>
+                    <a class="dropdown-item" href="#" data-bind="click:function(){$parent.moveSimpleRoutineDown($index());}, css:{disabled: $parent._exercisesToBeDone().length -1  == $index()}"><i class="fas fa-long-arrow-alt-down text-info"></i> Move down </a>
+                    <a class="dropdown-item" href="#" data-bind="click:$parent.addSimpleRoutineFromToDo"><i class="fas fa-plus fa-sm text-success" aria-hidden="true"></i> Repeat </a>
+                    <div role="separator" class="dropdown-divider"></div>
+                    <a class="dropdown-item " href="#" data-bind="click:function(){ $parent.removeSimpleRoutineFromToDo($index)}"><i class="fas fa-trash fa-sm text-danger" aria-hidden="true"></i> Remove </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- ko foreach:_exerciseMeasures-->
+            <!-- ko if: _exerciseMeasureType.measureType() == @((int)MeasureType.Weight)-->
+            <div class="form-group my-1 col-lg-3" data-bind="validationElement: _exerciseMeasureType.measureValue">
+              <label class="sr-only" data-bind="attr:{for:'measure_input_id_' + $index()}"></label>
+              <div class="input-group mx-1 pr-1">
+                <input type="number" min="0" class="form-control measure-value-input" data-bind="value: _exerciseMeasureType.measureValue, valueUpdate: ['input', 'afterkeydown'], attr: { placeholder: _exerciseMeasureType.measureDesciption, id:'measure_input_id_' + $index()}" aria-describedby="prPercentHelpBlock">
+                <span class="input-group-append">
+                  <span class="input-group-text" data-bind="text:_exerciseMeasureType.shortMeasureDescription()"></span>
+                </span>
+                <!-- ko if: _exerciseMeasureType._canSeePersonalRecord -->
+                <span class="w-100"></span>
+                <small id="prPercentHelpBlock" class="form-text text-muted" data-bind="text: _exerciseMeasureType.personalRecordPercent">
+
+                </small>
+                <!-- /ko -->
+              </div>
+            </div>
+            <!-- /ko -->
+            <!-- ko ifnot: _exerciseMeasureType.measureType() == @((int)MeasureType.Weight)-->
+            <div class="form-group my-1 col-lg-3" data-bind="validationElement: _exerciseMeasureType.measureValue">
+              <label class="sr-only" data-bind="attr:{for:'measure_input_id_' + $index()}"></label>
+              <div class="input-group mx-1 pr-1">
+                <input type="number" min="0" class="form-control measure-value-input" data-bind="value: _exerciseMeasureType.measureValue, valueUpdate: 'afterkeydown', attr: { placeholder: _exerciseMeasureType.measureDesciption, id:'measure_input_id_' + $index()}">
+                <span class="input-group-append">
+                  <span class="input-group-text" data-bind="text:_exerciseMeasureType.shortMeasureDescription()"></span>
+                </span>
+              </div>
+            </div>
+            <!-- /ko -->
+            <!-- /ko -->
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -35,7 +95,6 @@ export default class ExercisesListComponent extends Vue {
   workoutEdit: IWorkoutEditState;
 
   exerciseChange(selectedExerciseId: number) {
-    debugger;
     let workoutToAdd = this.workoutEdit.exercises.find(
       x => x.id == selectedExerciseId
     );
