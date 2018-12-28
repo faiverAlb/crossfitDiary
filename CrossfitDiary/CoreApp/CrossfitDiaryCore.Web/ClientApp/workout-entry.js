1d0c101a1b4fd9b35609c4c4b599ec191d3d1ad8
Albert Gizetdinov <faiver.alb@gmail.com>
2018-12-28 08:16:57 +0300

    Add emom

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-entry.js
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-entry.js
@@ -20,7 +20,7 @@ var routes = [
     { path: "/emom", component: EmomEditComponent },
     { path: "/e2mom", component: E2momEditComponent },
     { path: "/nft", component: NFTEditComponent },
-    { path: "/", component: AmrapEditComponent, redirect: "amrap" }
+    { path: "/", component: EmomEditComponent, redirect: "emom" }
 ];
 var vue = new Vue({
     el: "#manage-workout-page-container",
