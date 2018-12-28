17cbcf4b50dae9b936492bdc16cc9e6491dfcc4c
Albert Gizetdinov <faiver.alb@gmail.com>
2018-12-28 09:03:50 +0300

    Fix display of rest

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/workout-display-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/workout-display-component.vue
@@ -1,31 +1,46 @@
 ﻿<template>
-    <div>
-        <span class="workout-header d-flex justify-content-between">
-            <span class="workout-title">
-                <span>{{workoutViewModel.workoutTypeDisplay}}</span>:
-                <span v-if="workoutViewModel.IsForTime()">
-                    {{workoutViewModel.roundsCount}} rounds
-                </span>
-                <span v-if="workoutViewModel.IsAMRAP()">
-                    {{workoutViewModel.timeToWork}}
-                </span>
-            </span>
-            <span class="cap-time-info" v-if="workoutViewModel.timeCap">
-                Cap: {{workoutViewModel.timeCap}}
-            </span>
+  <div>
+    <span class="workout-header d-flex justify-content-between">
+      <span class="workout-title">
+        <span>{{workoutViewModel.workoutTypeDisplay}}</span>:
+        <span v-if="workoutViewModel.IsForTime()">
+          {{workoutViewModel.roundsCount}} rounds
         </span>
-        <div class="workout-exercises pl-3 pt-1" v-for="(exercise,index) in workoutViewModel.exercisesToDoList" :key="`${workoutViewModel.id}-${exercise.id}-${index}`">
-            <ExerciseDisplayComponent :model="exercise"></ExerciseDisplayComponent>
-            <div class="rest-between-rounds" v-if="workoutViewModel.restBetweenRounds">
-                <span>Rest: {{workoutViewModel.restBetweenRounds}}</span>
-            </div>
-        </div>
-        <div class="inner-workouts pl-3 pt-1">
-            <div class="inner-workout" v-for="childWorkout in workoutViewModel.children" :key="childWorkout.id">
-                <WorkoutDisplayComponent :workoutViewModel="childWorkout"></WorkoutDisplayComponent>
-            </div>
-        </div>
+        <span v-if="workoutViewModel.IsAMRAP()">
+          {{workoutViewModel.timeToWork}}
+        </span>
+      </span>
+      <span
+        class="cap-time-info"
+        v-if="workoutViewModel.timeCap"
+      >
+        Cap: {{workoutViewModel.timeCap}}
+      </span>
+    </span>
+    <div
+      class="workout-exercises pl-3 pt-1"
+      v-for="(exercise,index) in workoutViewModel.exercisesToDoList"
+      :key="`${workoutViewModel.id}-${exercise.id}-${index}`"
+    >
+      <ExerciseDisplayComponent :model="exercise"></ExerciseDisplayComponent>
+
     </div>
+    <div
+      class="rest-between-rounds pl-3 pt-1"
+      v-if="workoutViewModel.restBetweenRounds"
+    >
+      <span>Rest: {{workoutViewModel.restBetweenRounds}}</span>
+    </div>
+    <div class="inner-workouts pl-3 pt-1">
+      <div
+        class="inner-workout"
+        v-for="childWorkout in workoutViewModel.children"
+        :key="childWorkout.id"
+      >
+        <WorkoutDisplayComponent :workoutViewModel="childWorkout"></WorkoutDisplayComponent>
+      </div>
+    </div>
+  </div>
 
 </template>
 
