1f83eca8fdc3f00420d7284a5761a0c93d31f701
GizetdinovA <faiver.alb@gmail.com>
2019-01-08 14:05:37 +0300

    Add spinner for for time

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/fortime-edit-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/fortime-edit-component.vue
@@ -9,6 +9,7 @@
           ></ErrorAlertComponent>
         </div>
       </div>
+      
       <div class="pt-2 general-info-container">
         <div class="row">
           <div class="col-lg-3 time-cap-container pb-2">
@@ -155,6 +156,18 @@
               </b-input-group>
             </div>
           </div>
+          <div class="row">
+            <div class="offset-5">
+              <spinner 
+                :status="spinner.status"  
+                :size="spinner.size" 
+                :color="spinner.color"  
+                :depth="spinner.depth" 
+                :rotation="spinner.rotation"
+                :speed="spinner.speed">
+              </spinner>
+            </div>
+          </div>
           <button
             class="btn btn-success"
             v-on:click="logWorkout"
@@ -196,6 +209,9 @@ import { WorkoutType } from "../../../models/viewModels/WorkoutType";
 
 import ErrorAlertComponent from "../../error-alert-component.vue";
 import { ErrorAlertModel } from "../../../models/viewModels/ErrorAlertModel";
+import Spinner from "vue-spinner-component/src/Spinner.vue";
+import { SpinnerModel } from "./../../../models/viewModels/SpinnerModel";
+
 
 Vue.use(VeeValidate);
 declare var workouter: {
@@ -210,6 +226,7 @@ declare var workouter: {
     bFormInput,
     datePicker,
     bAlert,
+    Spinner,
     ErrorAlertComponent
   },
   directives: { mask }
@@ -218,6 +235,7 @@ export default class ForTimeEditComponent extends Vue {
   model: WorkoutViewModel = new WorkoutViewModel();
   toLogModel: ToLogWorkoutViewModel = new ToLogWorkoutViewModel();
   errorAlertModel: ErrorAlertModel = new ErrorAlertModel();
+  spinner:SpinnerModel = new SpinnerModel(true);
 
   mounted() {
     if (workouter != null && workouter.toLogWorkoutRawModel != null) {
@@ -243,12 +261,14 @@ export default class ForTimeEditComponent extends Vue {
         };
         let crossfitterService: CrossfitterService = new CrossfitterService();
 
+        this.spinner.activate();
         crossfitterService
           .createAndLogWorkout(model)
           .then(data => {
             window.location.href = "\\";
           })
           .catch(data => {
+            this.spinner.disable();
             this.errorAlertModel.setError(data.response.statusText);
           });
       }
