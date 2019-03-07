3a91df883d852529333a30b11faed94f50660486
Albert Gizetdinov <faiver.alb@gmail.com>
2019-03-07 09:55:20 +0300

    Add migrations for "Comment" feature

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using CrossfitDiaryCore.DAL.EF;
+
+namespace CrossfitDiaryCore.Web
+{
+    public class DbInitializer
+    {
+        public static void Seed(WorkouterContext context)
+        {
+//            context.Exercises
+//            using (var serviceScope = applicationBuilder.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+//                .CreateScope())
+//            {
+//                AppDbContext context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+//
+//                if (!context.Products.Any())
+//                {
+//                    // Seed Here
+//                }
+//
+//                context.SaveChanges();
+//            }
+        }
+    }
+}
--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/Program.cs
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CrossfitDiaryCore.DAL.EF;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace CrossfitDiaryCore.Web
@@ -14,10 +16,27 @@ namespace CrossfitDiaryCore.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<WorkouterContext>();
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .ConfigureAppConfiguration((context, builder) =>
