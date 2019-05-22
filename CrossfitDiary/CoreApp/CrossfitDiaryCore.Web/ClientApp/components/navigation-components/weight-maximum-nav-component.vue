77d2885cbb4128a1488a7810b1c22fe02df53883
Albert Gizetdinov <faiver.alb@gmail.com>
2019-05-22 21:11:28 +0300

    Add  WeightMaximumNavComponent

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/navigation-components/weight-maximum-nav-component.vue
@@ -0,0 +1,46 @@
+﻿<template>
+  <div>
+    <a
+      class="nav-link btn btn-info text-light d-inline-block d-md-none p-0"
+      data-toggle="collapse"
+      href="#collapseExample"
+      role="button"
+      aria-expanded="false"
+      aria-controls="collapseExample"
+    >
+      <div class="small-action-link-button">
+        <div class="icon-container">
+          <svg class="svg-inline w-18">
+            <use xlink:href="#trophy-icon"></use>
+          </svg>
+        </div>
+        <div class="text-container">MAX</div>
+      </div>
+      <span class="d-none d-md-inline">Vue Weight maximums</span>
+    </a>
+    <a
+      class="nav-link btn btn-info text-light d-none d-md-inline-block"
+      data-toggle="collapse"
+      href="#collapseExample"
+      role="button"
+      aria-expanded="false"
+      aria-controls="collapseExample"
+    >
+      <svg class="svg-inline w-18">
+        <use xlink:href="#trophy-icon"></use>
+      </svg>
+      <span class="d-none d-md-inline">Vue Weight maximums</span>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class WeightMaximumNavComponent extends Vue {}
+</script>
+
+<style>
+</style>
