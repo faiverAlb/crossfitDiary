761dfec12ea2f06a9686e47caf49b964f7390f88
Albert Gizetdinov <faiver.alb@gmail.com>
2019-12-01 14:39:01 +0300

    Add wods controller

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/Controllers/WodsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrossfitDiaryCore.Web.Controllers
+{
+    [Authorize]
+    public class WodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
