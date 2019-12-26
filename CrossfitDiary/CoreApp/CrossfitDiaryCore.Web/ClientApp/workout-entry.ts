8cdf78c6a48ac4c5fc694f9121af11fb263c8b2e
Albert Gizetdinov <faiver.alb@gmail.com>
2019-12-26 10:05:58 +0300

    Refactoring + fix style

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-entry.ts
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/workout-entry.ts
@@ -3,11 +3,8 @@ import VueRouter from "vue-router";
 
 import "./style/manage-workout.scss";
 
-import { dom } from "@fortawesome/fontawesome-svg-core";
-
 import Spinner from "vue-spinner-component/src/Spinner.vue";
-import { SpinnerModel } from "./models/viewModels/SpinnerModel";
-import { ErrorAlertModel } from "./models/viewModels/ErrorAlertModel";
+import {ErrorAlertModel} from "./models/viewModels/ErrorAlertModel";
 import ErrorAlertComponent from "./components/error-alert-component.vue";
 import WorkoutsNavigationComponent from "./components/manage-workout/workouts-navigation-component.vue";
 import AmrapEditComponent from "./components/manage-workout/workout-types/amrap-edit-component.vue";
@@ -18,53 +15,53 @@ import EmomEditComponent from "./components/manage-workout/workout-types/emom-ed
 import E2momEditComponent from "./components/manage-workout/workout-types/e2mom-edit-component.vue";
 import NFTEditComponent from "./components/manage-workout/workout-types/nft-edit-component.vue";
 import store from "./workout-edit-store/workout-store";
-import { ToLogWorkoutViewModel } from "./models/viewModels/ToLogWorkoutViewModel";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/fortime", component: ForTimeEditComponent /*, alias: "/"*/ },
-  { path: "/fortimen", component: ForTimeNEditComponent /*, alias: "/"*/ },
-  { path: "/amrap", component: AmrapEditComponent },
-  { path: "/amrapn", component: AmrapNEditComponent },
-  { path: "/emom", component: EmomEditComponent },
-  { path: "/e2mom", component: E2momEditComponent },
-  { path: "/nft", component: NFTEditComponent },
-  { path: "/", component: ForTimeEditComponent, redirect: "/fortime" }
+    {path: "/fortime", component: ForTimeEditComponent /*, alias: "/"*/},
+    {path: "/fortimen", component: ForTimeNEditComponent /*, alias: "/"*/},
+    {path: "/amrap", component: AmrapEditComponent},
+    {path: "/amrapn", component: AmrapNEditComponent},
+    {path: "/emom", component: EmomEditComponent},
+    {path: "/e2mom", component: E2momEditComponent},
+    {path: "/nft", component: NFTEditComponent},
+    {path: "/", component: ForTimeEditComponent, redirect: "/fortime"}
 ];
 
 let vue = new Vue({
-  el: "#manage-workout-page-container",
-  template: `
-   <div class="manage-workout-container my-2">
-      <div>
-        <div class="crossfitter-edit-workout-container">
-                <div class="col-md-12 add-new-workout-container workout-container">
-                  <div class="card">
-                    <div class="card-header">
-                        <workouts-navigation-component></workouts-navigation-component>
+    el: "#manage-workout-page-container",
+    template: `
+        <div class="manage-workout-container my-2">
+            <div>
+                <div class="crossfitter-edit-workout-container">
+                    <div class="col-md-12 add-new-workout-container workout-container">
+                        <div class="card">
+                            <div class="card-header">
+                                <workouts-navigation-component></workouts-navigation-component>
+                            </div>
+                            <div class="card-body pt-0">
+                                <router-view></router-view>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body pt-0">
-                      <router-view></router-view>
-                    </div>
-                  </div>
                 </div>
+            </div>
         </div>
-      </div>
-  </div>
     `,
-  store,
-  router: new VueRouter({ routes }),
-  components: {
-    Spinner,
-    ErrorAlertComponent,
-    WorkoutsNavigationComponent
-  },
-  data() {
-    // let test = new ToLogWorkoutViewModel().deserialize(workouter.toLogWorkoutRawModel);
-    return {
-      errorAlertModel: new ErrorAlertModel()
-    };
-  },
-  mounted() {}
+    store,
+    router: new VueRouter({routes}),
+    components: {
+        Spinner,
+        ErrorAlertComponent,
+        WorkoutsNavigationComponent
+    },
+    data() {
+        // let test = new ToLogWorkoutViewModel().deserialize(workouter.toLogWorkoutRawModel);
+        return {
+            errorAlertModel: new ErrorAlertModel()
+        };
+    },
+    mounted() {
+    }
 });
