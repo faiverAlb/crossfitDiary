d53ef362c22afc530180132ae8931025679308f5
Albert Gizetdinov <faiver.alb@gmail.com>
2019-12-26 09:21:20 +0300

    Refactoring

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/e2mom-edit-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/e2mom-edit-component.vue
@@ -205,39 +205,24 @@
     import {library} from "@fortawesome/fontawesome-svg-core";
     /* public components */
     import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Mixins, Vue} from "vue-property-decorator";
     import datePicker from "vue-bootstrap-datetimepicker";
     import {BAlert, BBadge, BButton, BFormInput, BFormTextarea, BModal, InputGroupPlugin} from "bootstrap-vue";
     import "pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css";
     import {mask} from "vue-the-mask";
-    import VeeValidate from "vee-validate";
 
     import Spinner from "vue-spinner-component/src/Spinner.vue";
-    import {IWorkoutEditState} from "../../../workout-edit-store/types";
-    import {State} from "vuex-class";
     /* app components */
-    import CrossfitterService from "../../../CrossfitterService";
     import ExercisesListComponent from "./exercises-list-component.vue";
     import ErrorAlertComponent from "../../error-alert-component.vue";
     import EditPlannedWorkoutComponent from "../edit-planned-workout-component.vue";
     /* models and styles */
-    import {WorkoutViewModel} from "../../../models/viewModels/WorkoutViewModel";
-    import {ToLogWorkoutViewModel} from "../../../models/viewModels/ToLogWorkoutViewModel";
     import {WorkoutType} from "../../../models/viewModels/WorkoutType";
-    import {ErrorAlertModel} from "../../../models/viewModels/ErrorAlertModel";
-    import {SpinnerModel} from "../../../models/viewModels/SpinnerModel";
-    import {WindowHelper} from "../../../helpers/WindowHelper";
+    import {WorkoutTypeComponent} from "./workoutTypeMixin";
 
     library.add(faClock, faHashtag, faCalendar);
 
     Vue.use(InputGroupPlugin);
-    Vue.use(VeeValidate);
-    const namespace: string = "workoutEdit";
-
-    declare var workouter: {
-        toLogWorkoutRawModel: ToLogWorkoutViewModel;
-        workoutViewModel: WorkoutViewModel;
-    };
 
     @Component({
         components: {
@@ -256,95 +241,9 @@
         },
         directives: {mask}
     })
-    export default class E2momEditComponent extends Vue {
-        model: WorkoutViewModel = new WorkoutViewModel();
-        toLogModel: ToLogWorkoutViewModel = new ToLogWorkoutViewModel();
-        errorAlertModel: ErrorAlertModel = new ErrorAlertModel();
-        spinner: SpinnerModel = new SpinnerModel(false);
-        $refs: {
-            logWorkoutModal: HTMLFormElement;
-        };
-        @State("workoutEdit")
-        workoutEdit: IWorkoutEditState;
-
+    export default class E2momEditComponent extends Mixins(WorkoutTypeComponent) {
         mounted() {
-            if (workouter != null && workouter.toLogWorkoutRawModel != null) {
-                this.model = workouter.toLogWorkoutRawModel.workoutViewModel;
-                this.toLogModel = workouter.toLogWorkoutRawModel;
-            } else if (workouter != null && workouter.workoutViewModel != null) {
-                this.model = workouter.workoutViewModel;
-            } else {
-                this.model.workoutType = WorkoutType.E2MOM;
-            }
-        }
-
-        planWorkoutAction(): void {
-            this.$validator.validate();
-
-            let scrollToErrors = this.$el.querySelector("[aria-invalid=true]");
-            if (scrollToErrors) {
-                WindowHelper.scrollToTargetAdjusted(scrollToErrors);
-            }
-
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    let crossfitterService: CrossfitterService = new CrossfitterService();
-                    this.spinner.activate();
-                    crossfitterService
-                        .createAndPlanWorkout(this.model)
-                        .then(data => {
-                            window.location.href = "\\";
-                        })
-                        .catch(data => {
-                            this.spinner.disable();
-                            this.errorAlertModel.setError(data.response.statusText);
-                        });
-                }
-            });
-        }
-
-        logWorkout() {
-            this.$validator.validate();
-
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    let workoutModel = this.model;
-                    const model = {
-                        newWorkoutViewModel: workoutModel,
-                        logWorkoutViewModel: this.toLogModel
-                    };
-                    let crossfitterService: CrossfitterService = new CrossfitterService();
-                    this.spinner.activate();
-
-                    crossfitterService
-                        .createAndLogWorkout(model)
-                        .then(data => {
-                            window.location.href = "\\";
-                        })
-                        .catch(data => {
-                            this.spinner.disable();
-                            this.errorAlertModel.setError(data.response.statusText);
-                        });
-                }
-            });
-        }
-
-        showLogWorkoutModal(): void {
-            this.$validator.validate();
-
-            let scrollToErrors = this.$el.querySelector("[aria-invalid=true]");
-            if (scrollToErrors) {
-                WindowHelper.scrollToTargetAdjusted(scrollToErrors);
-            }
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    this.$refs.logWorkoutModal.show();
-                }
-            });
-        }
-
-        hideLogModal(): void {
-            this.$refs.logWorkoutModal.hide();
+            this.model.workoutType = WorkoutType.E2MOM;
         }
     }
 </script>
