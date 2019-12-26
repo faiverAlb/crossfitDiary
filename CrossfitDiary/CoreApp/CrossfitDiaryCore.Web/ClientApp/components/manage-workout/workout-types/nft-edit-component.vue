741d1ef2ae48dd4b3bdfb61f3305ec3abb6618fb
Albert Gizetdinov <faiver.alb@gmail.com>
2019-12-26 10:07:41 +0300

    Refactoring

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/nft-edit-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/nft-edit-component.vue
@@ -18,7 +18,6 @@
 
                 <div class="comments-section">
                     <b-form-textarea
-                            size="sm"
                             :maxlength="150"
                             class="mt-2"
                             id="commentSection"
@@ -27,6 +26,7 @@
                             no-resize
                             placeholder="Note: ex. girls do max 30kg"
                             rows="2"
+                            size="sm"
                             type="text"
                             v-model="model.comment"
                     />
@@ -101,7 +101,6 @@
                             v-bind:class="{saving:spinner.status}"
                     >
                         <b-form-textarea
-                                size="sm"
                                 :maxlength="200"
                                 class="mt-2"
                                 id="logWorkoutCommentSection"
@@ -110,6 +109,7 @@
                                 no-resize
                                 placeholder="Note: ex. Holy sh*t! Will do it again!"
                                 rows="2"
+                                size="sm"
                                 type="text"
                                 v-model="toLogModel.comment"
                         />
@@ -151,17 +151,14 @@
     import {library} from "@fortawesome/fontawesome-svg-core";
     /* public components */
     import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Mixins, Vue} from "vue-property-decorator";
     import {BAlert, BBadge, BButton, BFormInput, BFormTextarea, BModal, InputGroupPlugin} from "bootstrap-vue";
     import datePicker from "vue-bootstrap-datetimepicker";
     import "pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css";
     import {mask} from "vue-the-mask";
     import VeeValidate from "vee-validate";
     import Spinner from "vue-spinner-component/src/Spinner.vue";
-    import {IWorkoutEditState} from "../../../workout-edit-store/types";
-    import {State} from "vuex-class";
     /* app components */
-    import CrossfitterService from "../../../CrossfitterService";
     import ExercisesListComponent from "./exercises-list-component.vue";
     import ErrorAlertComponent from "../../error-alert-component.vue";
     import EditPlannedWorkoutComponent from "../edit-planned-workout-component.vue";
@@ -169,9 +166,7 @@
     import {WorkoutViewModel} from "../../../models/viewModels/WorkoutViewModel";
     import {ToLogWorkoutViewModel} from "../../../models/viewModels/ToLogWorkoutViewModel";
     import {WorkoutType} from "../../../models/viewModels/WorkoutType";
-    import {ErrorAlertModel} from "../../../models/viewModels/ErrorAlertModel";
-    import {SpinnerModel} from "../../../models/viewModels/SpinnerModel";
-    import {WindowHelper} from "../../../helpers/WindowHelper";
+    import {WorkoutTypeComponent} from "./workoutTypeMixin";
 
     library.add(faClock, faHashtag, faCalendar);
 
@@ -201,98 +196,9 @@
         },
         directives: {mask}
     })
-    export default class NFTEditComponent extends Vue {
-        model: WorkoutViewModel = new WorkoutViewModel();
-        toLogModel: ToLogWorkoutViewModel = new ToLogWorkoutViewModel();
-        errorAlertModel: ErrorAlertModel = new ErrorAlertModel();
-        spinner: SpinnerModel = new SpinnerModel(false);
-
-        $refs: {
-            logWorkoutModal: HTMLFormElement;
-        };
-        @State("workoutEdit")
-        workoutEdit: IWorkoutEditState;
-
+    export default class NFTEditComponent extends Mixins(WorkoutTypeComponent) {
         mounted() {
-            if (workouter != null && workouter.toLogWorkoutRawModel != null) {
-                this.model = workouter.toLogWorkoutRawModel.workoutViewModel;
-                this.toLogModel = workouter.toLogWorkoutRawModel;
-            } else if (workouter != null && workouter.workoutViewModel != null) {
-                this.model = workouter.workoutViewModel;
-            } else {
-                this.model.workoutType = WorkoutType.NotForTime;
-            }
-        }
-
-        mutateData(): void {
-        }
-
-        planWorkoutAction(): void {
-            this.$validator.validate();
-
-            let scrollToErrors = this.$el.querySelector("[aria-invalid=true]");
-            if (scrollToErrors) {
-                WindowHelper.scrollToTargetAdjusted(scrollToErrors);
-            }
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    let crossfitterService: CrossfitterService = new CrossfitterService();
-                    this.spinner.activate();
-                    crossfitterService
-                        .createAndPlanWorkout(this.model)
-                        .then(data => {
-                            window.location.href = "\\";
-                        })
-                        .catch(data => {
-                            this.spinner.disable();
-                            this.errorAlertModel.setError(data.response.statusText);
-                        });
-                }
-            });
-        }
-
-        logWorkout() {
-            this.$validator.validate();
-
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    let workoutModel = this.model;
-                    const model = {
-                        newWorkoutViewModel: workoutModel,
-                        logWorkoutViewModel: this.toLogModel
-                    };
-                    let crossfitterService: CrossfitterService = new CrossfitterService();
-                    this.spinner.activate();
-
-                    crossfitterService
-                        .createAndLogWorkout(model)
-                        .then(data => {
-                            window.location.href = "\\";
-                        })
-                        .catch(data => {
-                            this.spinner.disable();
-                            this.errorAlertModel.setError(data.response.statusText);
-                        });
-                }
-            });
-        }
-
-        showLogWorkoutModal(): void {
-            this.$validator.validate();
-
-            let scrollToErrors = this.$el.querySelector("[aria-invalid=true]");
-            if (scrollToErrors) {
-                WindowHelper.scrollToTargetAdjusted(scrollToErrors);
-            }
-            this.$validator.validate().then(isValid => {
-                if (isValid) {
-                    this.$refs.logWorkoutModal.show();
-                }
-            });
-        }
-
-        hideLogModal(): void {
-            this.$refs.logWorkoutModal.hide();
+            this.model.workoutType = WorkoutType.NotForTime;
         }
     }
 </script>
