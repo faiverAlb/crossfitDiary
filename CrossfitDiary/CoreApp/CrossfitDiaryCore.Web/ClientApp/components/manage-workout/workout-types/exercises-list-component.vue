c62d5ba4f5db4114a72a2e34bb2400d6ae68be5e
Albert Gizetdinov <faiver.alb@gmail.com>
2020-01-05 12:31:05 +0300

    Add colors and styles for measures

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/exercises-list-component.vue
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/ClientApp/components/manage-workout/workout-types/exercises-list-component.vue
@@ -146,7 +146,48 @@
                                         v-model="measure.measureValue"
                                 />
                                 <b-input-group-append>
-                                    <b-input-group-text tag="span">
+                                    <b-input-group-text class="bg-secondary p-0" tag="span"
+                                                        v-if="measure.measureType == 1">
+                                        <span class="badge badge-secondary">
+                                                    {{measure.shortMeasureDescription}}
+                                        <font-awesome-icon :icon="['fas','walking']"
+                                                           size="lg"/>
+                                        </span>
+                                    </b-input-group-text>
+                                    <b-input-group-text class="bg-info p-0" tag="span"
+                                                        v-else-if="measure.measureType == 2">
+                                        <span class="badge badge-info">
+                                                    {{measure.shortMeasureDescription}}
+                                        <span>#</span>
+                                        </span>
+                                    </b-input-group-text>
+                                    <b-input-group-text class="bg-primary p-0" tag="span"
+                                                        v-else-if="measure.measureType == 3">
+                                        <span class="badge badge-primary">
+                                                    {{measure.shortMeasureDescription}}
+                                            <font-awesome-icon :icon="['fas','male']" size="lg" />
+                                        </span>
+                                    </b-input-group-text>
+                                    <b-input-group-text class="bg-warning p-0" tag="span"
+                                                        v-else-if="measure.measureType == 4">
+                                        <span class="badge badge-warning">
+                                        
+                                            cal's
+                                        <font-awesome-icon :icon="['fas','burn']"/>
+                                        </span>
+                                    </b-input-group-text>
+                                    <b-input-group-text class="bg-pink p-0" tag="span"
+                                                        v-else-if="measure.measureType == 8">
+                                        <span class="badge badge-pink">
+                                                    {{measure.shortMeasureDescription}}
+                                        <font-awesome-icon :icon="['fas','female']" size="lg"
+                                                           />
+                                        </span>
+                                    </b-input-group-text>
+
+                                    <b-input-group-text tag="span" v-else>
+
+
                                         {{measure.shortMeasureDescription}}
                                     </b-input-group-text>
                                 </b-input-group-append>
@@ -169,6 +210,11 @@
     import {faLongArrowAltDown} from "@fortawesome/free-solid-svg-icons/faLongArrowAltDown";
     import {faPlus} from "@fortawesome/free-solid-svg-icons/faPlus";
     import {faTrash} from "@fortawesome/free-solid-svg-icons/faTrash";
+    import {faBurn} from "@fortawesome/free-solid-svg-icons/faBurn";
+    import {faListOl} from "@fortawesome/free-solid-svg-icons/faListOl";
+    import {faWalking} from "@fortawesome/free-solid-svg-icons/faWalking";
+    import {faMale} from "@fortawesome/free-solid-svg-icons/faMale";
+    import {faFemale} from "@fortawesome/free-solid-svg-icons/faFemale";
     import {library} from "@fortawesome/fontawesome-svg-core";
     /* public components */
     import {Component, Prop, Vue} from "vue-property-decorator";
@@ -192,7 +238,7 @@
     import {IWorkoutEditState} from "../../../workout-edit-store/types";
     import "vue-select/src/scss/vue-select.scss";
 
-    library.add(faLongArrowAltUp, faLongArrowAltDown, faPlus, faTrash);
+    library.add(faLongArrowAltUp, faLongArrowAltDown, faPlus, faTrash, faBurn, faListOl, faWalking, faMale, faFemale);
     /**/
 
     Vue.use(InputGroupPlugin);
