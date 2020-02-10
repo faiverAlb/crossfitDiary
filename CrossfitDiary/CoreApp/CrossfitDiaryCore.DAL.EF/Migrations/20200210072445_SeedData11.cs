f8ed976ec777266e777a68e4d223e7ca2c52da88
faiverAlb <faiver.alb@gmail.com>
2020-02-10 08:41:13 +0000

    Release/v0.5.5

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.DAL.EF/Migrations/20200210072445_SeedData11.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrossfitDiaryCore.DAL.EF.Migrations
+{
+    public partial class SeedData11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Exercise",
+                columns: new[] { "Id", "Abbreviation", "Title" },
+                values: new object[] { 159, "BTN Split Jerk", "Behind the Neck Split Jerk" });
+
+            migrationBuilder.InsertData(
+                table: "ExerciseMeasure",
+                columns: new[] { "Id", "ExerciseId", "ExerciseMeasureTypeId" },
+                values: new object[] { 387, 159, 2 });
+
+            migrationBuilder.InsertData(
+                table: "ExerciseMeasure",
+                columns: new[] { "Id", "ExerciseId", "ExerciseMeasureTypeId" },
+                values: new object[] { 388, 159, 3 });
+
+            migrationBuilder.InsertData(
+                table: "ExerciseMeasure",
+                columns: new[] { "Id", "ExerciseId", "ExerciseMeasureTypeId" },
+                values: new object[] { 389, 159, 8 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 387);
+
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 388);
+
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 389);
+
+            migrationBuilder.DeleteData(
+                table: "Exercise",
+                keyColumn: "Id",
+                keyValue: 159);
+        }
+    }
+}
