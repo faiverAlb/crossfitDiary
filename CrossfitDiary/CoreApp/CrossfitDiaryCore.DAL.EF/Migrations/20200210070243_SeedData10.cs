269d9bbb948c80b01286c0f911dfbd81217e1e39
Albert Gizetdinov <faiver.alb@gmail.com>
2020-02-10 10:07:07 +0300

    Add seeds

---
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.DAL.EF/Migrations/20200210070243_SeedData10.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrossfitDiaryCore.DAL.EF.Migrations
+{
+    public partial class SeedData10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Exercise",
+                columns: new[] { "Id", "Abbreviation", "Title" },
+                values: new object[] { 157, "BTN Jerk", "Behind the Neck Jerk" });
+
+            migrationBuilder.InsertData(
+                table: "Exercise",
+                columns: new[] { "Id", "Abbreviation", "Title" },
+                values: new object[] { 158, "TU", "Tuck Ups" });
+
+            migrationBuilder.InsertData(
+                table: "ExerciseMeasure",
+                columns: new[] { "Id", "ExerciseId", "ExerciseMeasureTypeId" },
+                values: new object[,]
+                {
+                    { 383, 157, 2 },
+                    { 384, 157, 3 },
+                    { 385, 157, 8 },
+                    { 386, 158, 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 383);
+
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 384);
+
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 385);
+
+            migrationBuilder.DeleteData(
+                table: "ExerciseMeasure",
+                keyColumn: "Id",
+                keyValue: 386);
+
+            migrationBuilder.DeleteData(
+                table: "Exercise",
+                keyColumn: "Id",
+                keyValue: 157);
+
+            migrationBuilder.DeleteData(
+                table: "Exercise",
+                keyColumn: "Id",
+                keyValue: 158);
+        }
+    }
+}
