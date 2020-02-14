6ec12b3a93c81006e5059fb061f807bfe94de3c7
Albert Gizetdinov <faiver.alb@gmail.com>
2020-02-14 15:48:46 +0300

    Fix for correllation issue

--- CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/Startup.cs
+++ CrossfitDiary/CoreApp/CrossfitDiaryCore.Web/Startup.cs
@@ -28,14 +28,68 @@ namespace CrossfitDiaryCore.Web
 
         public IConfiguration Configuration { get; }
 
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                // TODO: Use your User Agent library of choice here. 
+                if (DisallowsSameSiteNone(userAgent))
+                {
+                    options.SameSite = SameSiteMode.Unspecified;
+                }
+            }
+        }
+
+        public static bool DisallowsSameSiteNone(string userAgent)
+        {
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking
+            // stack.
+            if (userAgent.Contains("CPU iPhone OS 12") ||
+                userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. 
+            // This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None, 
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions, 
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
             });
 
             string connectionString = Configuration.GetConnectionString("CrossfitDiaryDB_Core");
@@ -53,22 +107,25 @@ namespace CrossfitDiaryCore.Web
             //
             services.AddDbContext<WorkouterContext>(options => options.UseSqlServer(connectionString));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<WorkouterContext>()
-                .AddDefaultTokenProviders();
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<WorkouterContext>();
+
+            // services.AddIdentity<ApplicationUser, IdentityRole>()
+            // .AddEntityFrameworkStores<WorkouterContext>()
+            // .AddDefaultTokenProviders();
             //
             services.AddAuthentication()
-            .AddGoogle(googleOptions =>
-            {
-                googleOptions.ClientId = Configuration["oAuthConfiguration:google:clientId"];
-                googleOptions.ClientSecret = Configuration["oAuthConfiguration:google:clientSecret"];
-            })
-            .AddVkontakte(options =>
-            {
-                options.ClientId = Configuration["oAuthConfiguration:vkontakte:clientId"];
-                options.ClientSecret = Configuration["oAuthConfiguration:vkontakte:clientSecret"];
-                options.Scope.Add("email");
-            });
+                .AddGoogle(googleOptions =>
+                {
+                    googleOptions.ClientId = Configuration["oAuthConfiguration:google:clientId"];
+                    googleOptions.ClientSecret = Configuration["oAuthConfiguration:google:clientSecret"];
+                })
+                .AddVkontakte(options =>
+                {
+                    options.ClientId = Configuration["oAuthConfiguration:vkontakte:clientId"];
+                    options.ClientSecret = Configuration["oAuthConfiguration:vkontakte:clientSecret"];
+                    options.Scope.Add("email");
+                });
 
             services.Configure<IdentityOptions>(
                 options =>
@@ -80,12 +137,12 @@ namespace CrossfitDiaryCore.Web
                     options.Password.RequireUppercase = true;
                     options.Password.RequireLowercase = false;
                     options.Password.RequiredUniqueChars = 6;
-
+            
                     // Lockout settings
                     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
                     options.Lockout.MaxFailedAccessAttempts = 10;
                     options.Lockout.AllowedForNewUsers = true;
-
+            
                     // User settings
                     options.User.RequireUniqueEmail = true;
                 });
@@ -94,9 +151,9 @@ namespace CrossfitDiaryCore.Web
                 options =>
                 {
                     // Cookie settings
-
-                    // options.Cookie.HttpOnly = true;
-                    options.ExpireTimeSpan = TimeSpan.FromDays(150);
+            
+                    // options.Cookie.HttpOnly = true; 
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
                     options.LoginPath = "/Account/Login"; // If the LoginPath is not set here, ASP.NET Core will default to /Account/Login
                     options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here, ASP.NET Core will default to /Account/Logout
                     options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is not set here, ASP.NET Core will default to /Account/AccessDenied
@@ -115,19 +172,20 @@ namespace CrossfitDiaryCore.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCookiePolicy();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-//                app.UseExceptionHandler("/Error");
+                //                app.UseExceptionHandler("/Error");
             }
 
             app.UseHttpsRedirection();
-            app.UseCookiePolicy();
-
             app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthentication();
